296f09135835751d73a98915502051498aece2ad
yoonsiko <yoonsiko@stud.ntnu.no>
2023-04-11 18:17:48 +0200

    Created functions that calculates the hessian of the objective function

---
+++ J_ud.jl
@@ -0,0 +1,40 @@
+include("finite_diff.jl")
+include("nominal_case.jl")
+
+# Options for finite diff (eps = 1e-5)
+# 1: u1+h, n_O2
+# 2: u2+h, T_prePR
+# 3: u3+h, T_ATR
+# 4: d1+k, NG flow
+# 5: d2+k, P_el
+# 6: d3+k, P_H2
+# -1: u1-h, n_O2
+# -2: u2-h, T_prePR
+# -3: u3-h, T_ATR
+# -4: d1-k, NG flow
+# -5: d2-k, P_el
+# -6: d3-k, P_H2
+
+nominal_states, nom_f = nominal();
+eps = 1e-5
+function J_ud()
+    J_ud = zeros(3,3);
+    for i=1:3
+        for j=4:6
+            f1, h, k = finite_diff_2(i,j,eps); # f(x+h, y+k)
+            f2, h, k = finite_diff_1(i,eps) # f(x+h, y)
+            f3, h, k = finite_diff_1(-i,eps) # f(x-h, y)
+            f4, h, k = finite_diff_1(j,eps) # f(x, y+k)
+            f5, h, k = finite_diff_1(-j,eps) # f(x, y-k)
+            f6, h, k = finite_diff_2(-i, -j, eps); # f(x-h, y-k)
+            f_xy = (f1 - f2 - f3 - f4 - f5 + f6 + 2*nom_f)/(2*h*k)
+            J_ud[i,j-3] = f_xy
+        end
+    end
+    return J_ud
+end
+
+print(J_ud()) 
+[-2.034442890291113 -0.00040962366967068795 -0.0;
+ 0.00018942113500290043 0.27211266532288736 -0.0031574692743925425;
+  1.2105273985643835e-8 2.5144383393037338e-5 5.25875959818528e-7]
---
+++ J_uu.jl
@@ -0,0 +1,47 @@
+include("finite_diff.jl")
+include("nominal_case.jl")
+
+# Options for finite diff (eps = 1e-5)
+# 1: u1+h, n_O2
+# 2: u2+h, T_prePR
+# 3: u3+h, T_ATR
+# 4: d1+k, NG flow
+# 5: d2+k, P_el
+# 6: d3+k, P_H2
+# -1: u1-h, n_O2
+# -2: u2-h, T_prePR
+# -3: u3-h, T_ATR
+# -4: d1-k, NG flow
+# -5: d2-k, P_el
+# -6: d3-k, P_H2
+
+nominal_states, nom_f = nominal();
+eps = 1e-5
+function J_uu()
+    J_uu = zeros(3,3);
+    for i=1:3
+        for j=1:3
+            if i==j
+                f1, h, k = finite_diff_1(i,eps) # f(x+h, y)
+                f2, h, k = finite_diff_1(-i,eps) # f(x-h, y)
+                f_xx = (f1 - 2*nom_f + f2)/(h^2) # f(x+h,y) - 2f(x,y) + f(x-h,y) / h^2
+                J_uu[i,j] = f_xx
+            else
+                f1, h, k = finite_diff_2(i,j,eps); # f(x+h, y+k)
+                f2, h, k = finite_diff_1(i,eps) # f(x+h, y)
+                f3, h, k = finite_diff_1(-i,eps) # f(x-h, y)
+                f4, h, k = finite_diff_1(j,eps) # f(x, y+k)
+                f5, h, k = finite_diff_1(-j,eps) # f(x, y-k)
+                f6, h, k = finite_diff_2(-i, -j, eps); # f(x-h, y-k)
+                f_xy = (f1 - f2 - f3 - f4 - f5 + f6 + 2*nom_f)/(2*h*k)
+                J_uu[i,j] = f_xy
+            end
+        end
+    end
+    return J_uu
+end
+
+print(J_uu()) 
+[-7.460756212456425 -2.1357095953626186 -279.49599111708557;
+ -2.1357096398458104 -98.18393713138755 -0.004659896230874663;
+  -279.49599111708557 -0.004659896230874663 -0.0007855014350343562]
